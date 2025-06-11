de4b2e7e7971c17fc8f6c205998405d88a76a77a
akshat-25 <akshatparakh@Akshats-MacBook-Air.local>
2025-06-11 09:48:26 +0530

    Added more tokens and extended the lexer

--- src/lexer/lexer_test.go
+++ src/lexer/lexer_test.go
@@ -4,22 +4,31 @@ import (
 	"testing"
 
 	"loom/src/token"
-
 )
 
-func TestNextToken(t *testing.T)  {
+func TestNextToken(t *testing.T) {
 	input := `let five = 5;
-	let ten = 10;
-	
-	let add = fn(x, y) {
-		x + y;
-	};
+let ten = 10;
 
-	let result = add(five, ten);
-	`
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+10 == 10;
+10 != 9;`
 
 	tests := []struct {
-		expextedType token.TokenType
+		expextedType    token.TokenType
 		expectedLiteral string
 	}{
 		{token.LET, "let"},
@@ -58,6 +67,43 @@ func TestNextToken(t *testing.T)  {
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 
@@ -65,7 +111,7 @@ func TestNextToken(t *testing.T)  {
 
 	for i, tt := range tests {
 		tok := l.NextToken()
-		
+
 		if tok.Type != tt.expextedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expextedType, tok.Type)
 		}
